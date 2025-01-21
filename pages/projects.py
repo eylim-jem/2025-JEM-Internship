c8ba52d5a5aa5329f76cd6a3a12ad2253a2612c5
Kiara-Jemena <kiara.zammit@jemena.com.au>
2025-01-22 10:46:39 +1100

    Update projects.py

--- pages/projects.py
+++ pages/projects.py
@@ -7,6 +7,27 @@ st.set_page_config(
     layout="centered"
 )
 
+st.write(
+    """
+    As a whole, the growth in maximum demand across JEN’s network is progressively increasing, with the total 
+underlying (excluding large customer load forecast) network 50% POE maximum demand forecast to grow at an 
+average rate of 1.8% per annum over the next five years (2024-25 to 2028-29). This forecast is presented in 
+
+As a result of this 
+urban sprawl and the extension of the urban growth boundary, JEN expects to see continued strong growth in the 
+areas currently supplied by the Kalkallo, Sunbury (7.24% p.a.), Sydenham (8.32% p.a.), Somerton (6.80% p.a.), and Coolaroo (4.37% p.a.) 
+zone substations.
+
+JEN is forecasting relatively high 
+growth in maximum demand for areas currently supplied by Coburg North (6.06% p.a.), East Preston (6.49%), 
+Footscray East (6.55% p.a.), Fairfield (8.57% p.a.), Flemington (6.55% p.a.), Footscray West (8.31% p.a.), North 
+Essendon (5.07% p.a.), Preston (6.49% p.a.), and Somerton (6.80% p.a.) zone substations
+    """
+)
+# ---------------------------
+# Main Title
+# ---------------------------
+st.title("Projects")
 # ---------------------------
 # Main Title
 # ---------------------------
